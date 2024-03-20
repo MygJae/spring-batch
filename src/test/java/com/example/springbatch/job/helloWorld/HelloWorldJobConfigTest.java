1e3b70e7a5242fc5383534c4bb60b99d294d1dd2
MygJae <50861514+mygjae@users.noreply.github.com>
2024-03-20 10:53:36 +0900

    테스트 코드, 스케쥴러

---
+++ src/test/java/com/example/springbatch/job/helloWorld/HelloWorldJobConfigTest.java
@@ -0,0 +1,38 @@
+package com.example.springbatch.job.helloWorld;
+
+import com.example.springbatch.SpringBatchTestConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {SpringBatchTestConfig.class, HelloWorldJobConfig.class})
+@ActiveProfiles("test")
+@SpringBatchTest
+class HelloWorldJobConfigTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Test
+    public void success() throws Exception{
+        //when
+        JobExecution execution = jobLauncherTestUtils.launchJob();
+
+        //then
+        Assertions.assertEquals(execution.getExitStatus(), ExitStatus.COMPLETED);
+    }
+
+
+}
+
